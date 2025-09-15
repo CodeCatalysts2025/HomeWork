dae761703f61b87fb0a6f065f63e3bb1b2b987e9
buynaa <tserendagvabuyanzayaa@Buynaa-MacBook-Pro.local>
2025-09-15 18:09:24 +0800

    bugdin ustgsn

--- back/src/routes/user.route.ts
+++ back/src/routes/user.route.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import { CreateUser } from "../controllers/user";
-import { GetUserById } from "../controllers/user/get-user";
-import { GetUsers } from "../controllers/user/get-users";
-
 const userRouter = Router();
 
-userRouter.post("/", CreateUser).get("/:id", GetUserById).get("/", GetUsers);
+userRouter.get("/", (req, res) => {
+  res.send("User route");
+});
 
 export default userRouter;
